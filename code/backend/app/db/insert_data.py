3b48ff5abbf2c3f847da2e0c82688e9c353d118d
rheekang <rkang160@gmail.com>
2025-04-23 02:38:48 -0400

    cleaned and normalized db

--- code/backend/app/db/insert_data.py
+++ code/backend/app/db/insert_data.py
@@ -1,3 +1,5 @@
+import json
+
 import psycopg2
 category_keywords = []
 with open("food_categories.txt", "r") as f:
@@ -6,31 +8,157 @@ with open("food_categories.txt", "r") as f:
 
 #derived from all_categories.txt
 def create_business_db(connection, data):
-    for a in data[:400]:  # Limit for testing
+    for a in data:
         categories = a.get("categories")
         if not categories:
             continue
         if any(keyword in categories for keyword in category_keywords):
             with connection.cursor() as cursor:
-                print(a["name"])
-                #insert_core_business(cursor, [a])
-                #insert_category_business(cursor, a['business_id'], categories)
+                insert_core_business(cursor, [a])
+                insert_category_business(cursor, a['business_id'], categories)
+                insert_attributes_business(cursor, a['business_id'], a['attributes'])
+                if a.get('BusinessParking'):
+                    insert_parking_data(cursor, a['business_id'], a['BusinessParking'])
+                if a.get('Ambience'):
+                    insert_ambience_data(cursor, a['business_id'], a['ambience'])
+                if a.get('GoodForMeal'):
+                    insert_best_nights(cursor, a['business_id'], a['GoodForMeal'])
     connection.commit()
 
-def insert_core_business(connection, data):
+def insert_core_business(cursor, data):
     template = [(a['business_id'], a['name'], a['address'],
                     a['city'], a['state'], a['postal_code'],
                     a['latitude'], a['longitude'], a['stars'],
                     a['review_count'], a['is_open']) for a in data]
-    connection.executemany("INSERT INTO restaurant(id, name, address,"
+    cursor.executemany("INSERT INTO restaurant(id, name, address,"
                            "city, state, postal_code, latitude, longitude,"
                            "stars, review_count,"
                            " is_open ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", template)
 
-# def insert_category_business(connection, data):
-#     template = []
-#     for a in data:
-#         template = [(a['business_id'],a['categories']) for a in data]
-#     for a in template:
-#         connection.execute("INSERT INTO restaurant_categories(business_id, "
-#                            "name) VALUES (%s,%s)", template)
+
+def insert_category_business(cursor, business_id, categories):
+    if isinstance(categories, str):
+        categories = categories.split(",")
+    template = [(business_id, category) for category in categories]
+    cursor.executemany("""
+        INSERT INTO restaurant_categories(business_id, category)
+        VALUES (%s, %s)
+    """, template)
+
+
+def str_to_bool(val):
+    if isinstance(val, bool):
+        return val
+    if isinstance(val, str):
+        val = val.strip().lower()
+        return val in ['true', 't', 'yes', 'y', '1']
+    return False
+def str_to_varchar(val):
+    if isinstance(val, str):
+        return val.strip("u'").strip().lower()
+
+def insert_attributes_business(cursor, business_id, data):
+    if not data:
+        return
+
+    template = (
+        business_id,
+        str_to_bool(data.get('ByAppointmentOnly')),
+        str_to_bool(data.get('BusinessAcceptsCreditCards')),
+        str_to_bool(data.get('BikeParking')),
+        data.get('RestaurantsPriceRange2'),
+        str_to_bool(data.get('CoatCheck')),
+        str_to_bool(data.get('RestaurantsTakeOut')),
+        str_to_bool(data.get('RestaurantsDelivery')),
+        str_to_bool(data.get('Caters')),
+        str_to_varchar(data.get('WiFi')),
+        str_to_bool(data.get('WheelchairAccessible')),
+        str_to_bool(data.get('HappyHour')),
+        str_to_bool(data.get('OutdoorSeating')),
+        str_to_bool(data.get('HasTV')),
+        str_to_bool(data.get('RestaurantsReservations')),
+        str_to_bool(data.get('DogsAllowed')),
+        str_to_varchar(data.get('Alcohol')),
+        str_to_bool(data.get('GoodForKids')),
+        str_to_varchar(data.get('RestaurantsAttire')),
+        str_to_bool(data.get('RestaurantsTableService')),
+        str_to_bool(data.get('DriveThru')),
+        str_to_varchar(data.get('NoiseLevel')),
+        str_to_bool(data.get('BusinessAcceptsBitcoin')),
+        str_to_bool(data.get('Smoking')),
+        str_to_bool(data.get('Music')),
+        str_to_bool(data.get('GoodForDancing')),
+        str_to_bool(data.get('AcceptsInsurance')),
+        str_to_bool(data.get('BYOB')),
+        str_to_bool(data.get('Corkage')),
+        str_to_bool(data.get('BYOBCorkage')),
+        str_to_varchar(data.get('HairSpecializesIn')),
+        str_to_bool(data.get('Open24Hours')),
+        str_to_bool(data.get('RestaurantsCounterService')),
+        data.get('AgesAllowed'),
+        data.get('DietaryRestrictions')
+    )
+    cursor.executemany("""
+        INSERT INTO restaurant_attributes(business_id,
+                                          by_appointment_only, accept_credit_cards, bike_parking,
+                                          restaurant_price_range, coat_check, take_out, delivery,
+                                          caters, wifi, wheelchair,
+                                          happy_hour, outdoor_seating, tv, reservation, dogs_allowed,
+                                          alcohol, good_for_kids, restaurants_attire,
+                                          restaurant_table_service, drive_thru, noise_level,
+                                          accepts_bitcoin, smoking, music,
+                                          good_for_dancing, accepts_insurance,
+                                          byob, corkage, byob_corkage, hair_specializes_in,
+                                          open_24_hours, restaurant_counter_services, ages_allowed,
+                                          dietary_restrictions)
+        VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, [template])
+
+def insert_parking_data(cursor, business_id, parking_data):
+    if not parking_data:
+        return
+    template = [
+        (business_id, parking_type, available)
+        for parking_type, available in parking_data.items()
+    ]
+    cursor.executemany("""
+        INSERT INTO restaurant_parking(business_id, parking_type, available)
+        VALUES (%s, %s, %s)
+    """, template)
+
+
+def insert_best_nights(cursor, business_id, best_nights):
+    if not best_nights:
+        return
+    nights = [(business_id, day) for day, is_true in best_nights.items() if is_true == True]
+    if nights:
+        cursor.executemany("""
+            INSERT INTO restaurant_best_nights(business_id, day_of_week)
+            VALUES (%s, %s)
+        """, nights)
+
+
+def insert_ambience_data(cursor, business_id, ambience_data):
+    if not ambience_data:
+        return
+    template = [(business_id, vibe) for vibe, is_true in ambience_data.items() if is_true == True]
+
+    cursor.executemany("""
+        INSERT INTO restaurant_ambience(business_id, vibe)
+        VALUES (%s, %s)
+    """, template)
+
+
+def insert_dynamic_attributes(cursor, business_id, attributes):
+    if not attributes:
+        return
+    template = [
+        (business_id, key, json.dumps(value))  # JSON encode the value to store in JSONB column
+        for key, value in attributes.items()
+    ]
+
+    cursor.executemany("""
+        INSERT INTO restaurant_dynamic_attributes(business_id, attribute_key, attribute_value)
+        VALUES (%s, %s, %s)
+    """, template)
--- code/backend/app/db/schema.sql
+++ code/backend/app/db/schema.sql
@@ -1,3 +1,25 @@
+--\i /Users/rheekang/docker_projects/crAIvings/code/backend/app/db/schema.sql
+-- write above the first time to create
+
+
+-- db_craivings=> \dt
+--                     List of relations
+--  Schema |         Name          | Type  |     Owner
+-- --------+-----------------------+-------+----------------
+--  public | restaurant            | table | craivings_user
+--  public | restaurant_attributes | table | craivings_user
+--  public | restaurant_categories | table | craivings_user
+--  public | restaurant_hours      | table | craivings_user
+
+--in case pre-existing
+DROP TABLE IF EXISTS restaurant_attributes CASCADE;
+DROP TABLE IF EXISTS restaurant_hours CASCADE;
+DROP TABLE IF EXISTS restaurant_categories CASCADE;
+DROP TABLE IF EXISTS restaurant CASCADE;
+DROP TABLE IF EXISTS restaurant_ambience CASCADE;
+DROP TABLE IF EXISTS restaurant_parking CASCADE;
+DROP TABLE IF EXISTS restaurant_good_for_meal CASCADE;
+DROP TABLE IF EXISTS restaurant_dynamic_attributes CASCADE;
 CREATE TABLE restaurant(
     id VARCHAR PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -9,7 +31,7 @@ CREATE TABLE restaurant(
     longitude FLOAT NOT NULL,
     stars FLOAT NOT NULL,
     review_count INT NOT NULL,
-    is_open BOOLEAN NOT NULL
+    is_open INTEGER NOT NULL
 );
 
 CREATE TABLE restaurant_categories (
@@ -27,44 +49,75 @@ CREATE TABLE restaurant_hours (
 );
 
 CREATE TABLE restaurant_attributes(
-    business_id VARCHAR REFERENCES restaurant(id),
-    by_appointment_only VARCHAR,
-    accept_credit_cards VARCHAR,
-    bike_parking VARCHAR,
+    business_id VARCHAR PRIMARY KEY REFERENCES restaurant(id),
+    by_appointment_only BOOLEAN,
+    accept_credit_cards BOOLEAN,
+    bike_parking BOOLEAN,
     restaurant_price_range VARCHAR,
-    coat_check VARCHAR,
-    take_out VARCHAR,
-    delivery VARCHAR,
-    caters VARCHAR,
+    coat_check BOOLEAN,
+    take_out BOOLEAN,
+    delivery BOOLEAN,
+    caters BOOLEAN,
     wifi VARCHAR,
-    business_parking VARCHAR,
-    wheelchair VARCHAR,
-    happy_hour VARCHAR,
-    outdoor_seating VARCHAR,
-    tv VARCHAR,
-    reservation VARCHAR,
-    dogs_allowed VARCHAR,
+    wheelchair BOOLEAN,
+    happy_hour BOOLEAN,
+    outdoor_seating BOOLEAN,
+    tv BOOLEAN,
+    reservation BOOLEAN,
+    dogs_allowed BOOLEAN,
     alcohol VARCHAR,
-    good_for_kids VARCHAR,
+    good_for_kids BOOLEAN,
     restaurants_attire VARCHAR,
-    ambience VARCHAR,
-    restaurant_table_service VARCHAR,
-    drive_thru VARCHAR,
+    restaurant_table_service BOOLEAN,
+    drive_thru BOOLEAN,
     noise_level VARCHAR,
     good_for_meal VARCHAR,
-    accepts_bitcoin VARCHAR,
-    smoking VARCHAR,
-    music VARCHAR,
-    good_for_dancing VARCHAR,
-    accepts_insurance VARCHAR,
+    accepts_bitcoin BOOLEAN,
+    smoking BOOLEAN,
+    music BOOLEAN,
+    good_for_dancing BOOLEAN,
+    accepts_insurance BOOLEAN,
     best_nights VARCHAR,
-    byob VARCHAR,
-    corkage VARCHAR,
-    byob_corkage VARCHAR,
+    byob BOOLEAN,
+    corkage BOOLEAN,
+    byob_corkage BOOLEAN,
     hair_specializes_in VARCHAR,
-    open_24_hours VARCHAR,
-    restaurant_counter_services VARCHAR,
+    open_24_hours BOOLEAN,
+    restaurant_counter_services BOOLEAN,
     ages_allowed VARCHAR,
     dietary_restrictions VARCHAR,
     dynamic_attributes JSONB
-)
+);
+
+CREATE TABLE restaurant_best_nights(
+    business_id VARCHAR REFERENCES restaurant(id),
+    day_of_week VARCHAR,
+    PRIMARY KEY (business_id, day_of_week)
+);
+
+CREATE TABLE restaurant_parking (
+    business_id VARCHAR REFERENCES restaurant(id),
+    parking_type VARCHAR,
+    available BOOLEAN,
+    PRIMARY KEY (business_id, parking_type)
+);
+
+CREATE TABLE restaurant_ambience (
+    business_id VARCHAR REFERENCES restaurant(id),
+    vibe VARCHAR,
+    PRIMARY KEY (business_id, vibe)
+);
+
+
+CREATE TABLE restaurant_good_for_meal (
+    business_id VARCHAR REFERENCES restaurant(id),
+    occasion VARCHAR,
+    PRIMARY KEY (business_id, occasion)
+);
+
+CREATE TABLE restaurant_dynamic_attributes (
+    business_id VARCHAR REFERENCES restaurant(id),
+    attribute_key VARCHAR,
+    attribute_value JSONB,
+    PRIMARY KEY (business_id, attribute_key)
+);
